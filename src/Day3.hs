1f2b323dfcecc7a1627a82bd0f2f07410dc43354
Lily Rose <lilyrose2798@gmail.com>
2021-12-03 13:29:15 +0000

    Remove parens

--- src/Day3.hs
+++ src/Day3.hs
@@ -19,7 +19,7 @@ solveOne = (mulBools <*> map not) . map hasMoreFalses . transpose
 rec :: Bool -> [Bool] -> [[Bool]] -> [Bool]
 rec _ a ([]:_) = reverse a
 rec _ a (c:[]) = reverse a ++ c
-rec i a cs = ((rec i) <$> (: a) <*> (\x -> map tail $ filter ((== x) . head) cs)) ((== i) $ hasMoreFalses $ map head cs)
+rec i a cs = (rec i <$> (: a) <*> (\x -> map tail $ filter ((== x) . head) cs)) ((== i) $ hasMoreFalses $ map head cs)
 
 solveTwo :: [[Bool]] -> Int
 solveTwo = mulBools <$> rec True [] <*> rec False []
