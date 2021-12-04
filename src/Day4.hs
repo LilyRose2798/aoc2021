8d2eb60abb274f1cab57963dbd34c63af46b184e
Lily Rose <lilyrose2798@gmail.com>
2021-12-04 13:17:55 +0000

    Add Board type

--- src/Day4.hs
+++ src/Day4.hs
@@ -7,24 +7,25 @@ import qualified Data.Set as Set
 import Data.Set (Set)
 
 type BingoLine = Set Int
+type Board = [BingoLine]
 type DrawSet = Set Int
 type Draws = [Int]
 
-parse :: String -> (Draws, [[BingoLine]])
+parse :: String -> (Draws, [Board])
 parse = (\(n:bs) -> (map read $ splitOn "," n, map (map Set.fromList . ((++) <*> transpose) . map (map read . words) . lines) bs)) . splitOn "\n\n"
 
-hasBingo :: DrawSet -> [BingoLine] -> Bool
+hasBingo :: DrawSet -> Board -> Bool
 hasBingo = any . flip Set.isSubsetOf
 
-score :: Draws -> [BingoLine] -> Int
+score :: Draws -> Board -> Int
 score ds bls = last ds * sum (Set.difference (Set.unions bls) (Set.fromList ds))
 
-solveOne :: (Draws, [[BingoLine]]) -> Int
+solveOne :: (Draws, [Board]) -> Int
 solveOne (ds, blss) = (score <*> (fromJust . flip find blss . hasBingo . Set.fromList)) $ fromJust $ find (flip any blss . hasBingo . Set.fromList) (inits ds)
 
-rec :: [Draws] -> [[BingoLine]] -> Int
+rec :: [Draws] -> [Board] -> Int
 rec dss (bls:[]) = flip score bls $ fromJust $ find ((flip hasBingo) bls . Set.fromList) dss
 rec (ds:dss) blss = rec dss (filter (not . hasBingo (Set.fromList ds)) blss)
 
-solveTwo :: (Draws, [[BingoLine]]) -> Int
+solveTwo :: (Draws, [Board]) -> Int
 solveTwo = uncurry (rec . inits)
