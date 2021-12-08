738742525b7663f0ef39e976dfd8afaeb3e15ed7
Lily Rose <lilyrose2798@gmail.com>
2021-12-08 15:04:55 +0000

    Remove redundant brackets

--- src/Day6.hs
+++ src/Day6.hs
@@ -7,7 +7,7 @@ parse :: String -> [Int]
 parse = map (subtract 1 . length) . group . sort . (++ [0..8]) . map read . splitOn ","
 
 simulate :: [Int] -> [Int]
-simulate (n:ns) = take 6 ns ++ (\(n':ns') -> (n + n' : ns')) (drop 6 ns) ++ [n]
+simulate (n:ns) = take 6 ns ++ (\(n':ns') -> n + n' : ns') (drop 6 ns) ++ [n]
 
 popAt :: Int -> [Int] -> Int
 popAt n = sum . (!! n) . iterate simulate
