53f13eca6654eeee800b37420e9633247335bf89
Lily Rose <lilyrose2798@gmail.com>
2021-12-08 14:11:36 +0000

    Add solution timing capabilities

--- src/Main.hs
+++ src/Main.hs
@@ -8,6 +8,8 @@ import qualified Data.ByteString.UTF8 as BSU
 import Data.Conduit ((.|), runConduitRes)
 import Data.Conduit.Combinators (sinkFile)
 import Network.HTTP.Simple
+import System.TimeIt (timeItT)
+import Text.Printf (printf)
 
 import qualified Day1
 import qualified Day2
@@ -19,7 +21,7 @@ import qualified Day7
 import qualified Day8
 
 solve :: (Show b, Show c) => (String -> a) -> (a -> b) -> (a -> c) -> String -> String
-solve parse solveOne solveTwo = ((\x y -> "Part One = " ++ show x ++ " / Part Two = " ++ show y) <$> solveOne <*> solveTwo) . parse
+solve parse solveOne solveTwo = ((\x y -> "Part One: " ++ show x ++ "\nPart Two: " ++ show y) <$> solveOne <*> solveTwo) . parse
 
 solvers = 
   [ solve Day1.parse Day1.solveOne Day1.solveTwo
@@ -39,10 +41,16 @@ downloadInput n = do
   runConduitRes $ httpSource (addRequestHeader "Cookie" cookie req) getResponseBody .| sinkFile ("input/day" ++ show n ++ ".txt")
 
 readInput :: Int -> IO String
-readInput n = catch (readFile ("input/day" ++ show n ++ ".txt")) (\e -> const (downloadInput n >>= const (readInput n)) (e :: IOException))
+readInput n = catch readFileN (\e -> const (downloadInput n >>= const readFileN) (e :: IOException))
+  where readFileN = readFile ("input/day" ++ show n ++ ".txt")
 
-solveAll :: IO String
-solveAll = fmap unlines $ mapM (\(s, n) -> ((("Day " ++ show n ++ ": ") ++) . s) <$> readInput n) $ zip solvers [1..]
+runDay :: (String -> String) -> Int -> IO ()
+runDay s n = do
+  i <- readInput n
+  (t, _) <- timeItT $ putStrLn ("Day " ++ show n ++ "\n" ++ s i)
+  printf "CPU Time: %.3fs\n\n" t
 
 main :: IO ()
-main = solveAll >>= putStrLn
+main = do
+  (t, _) <- timeItT $ sequence_ (map (uncurry runDay) (zip solvers [1..]))
+  printf "Total CPU Time: %.3fs\n" t
