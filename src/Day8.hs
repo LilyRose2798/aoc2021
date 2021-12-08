4501877de6802f3e5d83b18f9f0020a17ea1fb49
Lily Rose <lilyrose2798@gmail.com>
2021-12-08 09:38:39 +0000

    Refactor day 8

--- src/Day8.hs
+++ src/Day8.hs
@@ -9,20 +9,20 @@ import Data.Digits (unDigits)
 import qualified Data.Set as Set
 import qualified Data.Map.Strict as Map
 
-segmentsToFreqSumFunc :: [String] -> String -> Int
-segmentsToFreqSumFunc = (\m -> sum . map (m Map.!)) . Map.fromList . zip ['a'..'g'] . map length . group . sort . join
+segmentsToFreqSumBy :: [String] -> String -> Int
+segmentsToFreqSumBy = (\m -> sum . map (m Map.!)) . Map.fromList . zip ['a'..'g'] . map length . group . sort . join
 
 digitSegments :: [String]
 digitSegments = ["abcefg", "cf", "acdeg", "acdfg", "bcdf", "abdfg", "abdefg", "acf", "abcdefg", "abcdfg"]
 
 freqSumMap :: Map.Map Int Int
-freqSumMap = Map.fromList (zip (map (segmentsToFreqSumFunc digitSegments) digitSegments) [0..])
+freqSumMap = Map.fromList (zip (map (segmentsToFreqSumBy digitSegments) digitSegments) [0..])
 
 parse :: String -> [([String], [String])]
 parse = map (both words . fromJust . stripInfix " | ") . lines
 
 solve :: ([String], [String]) -> [Int]
-solve (xs, ys) = map ((freqSumMap Map.!) . segmentsToFreqSumFunc xs) ys
+solve (xs, ys) = map ((freqSumMap Map.!) . segmentsToFreqSumBy xs) ys
 
 solveOne :: [([String], [String])] -> Int
 solveOne = length . filter (`Set.member` (Set.fromList [1, 4, 7, 8])) . (>>= solve)
